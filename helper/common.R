25b672a9e2f6121684e933a1e028b6b5de7e9257
Mike Frank <mcfrank@stanford.edu>
2024-08-28 13:47:18 -0700

    devo analysis

--- helper/common.R
+++ helper/common.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(lme4)
 library(knitr) # for kable
 # library(ggrepel)
-# library(ggthemes)
+library(ggthemes)
 # library(viridis)
 # library(cowplot)
 # remotes::install_github("jmgirard/agreement")
@@ -17,3 +17,12 @@ knitr::opts_chunk$set(cache.extra = knitr::rand_seed, cache = TRUE,
                       message=FALSE, warning=FALSE, error=FALSE)
 options(dplyr.summarise.inform = FALSE)
 
+
+.font <- "Source Sans Pro"
+theme_set(theme_bw(base_size = 14, base_family = .font))
+theme_update(panel.grid = element_blank(),
+             strip.background = element_blank(),
+             legend.key = element_blank(),
+             panel.border = element_blank(),
+             axis.line = element_line(),
+             strip.text = element_text(face = "bold"))
